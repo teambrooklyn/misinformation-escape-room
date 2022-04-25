43d225ce4fae540d1a8cbe74b318c7b938d2744a
MollyWu <molly729@uw.edu>
2022-04-24 22:58:39 -0700

    fixed positions for puzzle 3, but idk how to fix the close button

--- src/shared/puzzles/puzzle3/style.js
+++ src/shared/puzzles/puzzle3/style.js
@@ -3,8 +3,8 @@ const puzzle3 = require("../../../rooms/euphorigen/json/puzzle3.json");
 
 const styles = () => ({
   background: {
-    backgroundColor: "#E5E5E5",
-    height: "100vh !important"
+    backgroundColor: "#E5E5E5"
+    // height: "100vh !important"
   },
   backgroundImg: {
     padding: "4em 4em 8em 4em",
@@ -12,6 +12,7 @@ const styles = () => ({
     backgroundPosition: "center top",
     backgroundRepeat: "no-repeat",
     backgroundSize: "contain",
+    position:"relative"
   },
   content: {
     margin: "-2em -2em 0 0"
@@ -21,6 +22,11 @@ const styles = () => ({
     height: "729px",
     position: "relative"
   },
+  callout: {
+    position:"relative",
+    left:"30rem"
+  },
+
   instructions: {
     width: "350px",
     background: "rgba(112, 112, 112, 0.5)",
@@ -31,9 +37,14 @@ const styles = () => ({
     color: "black",
     fontSize: "1rem",
     position: "relative",
-    top: "30vh",
-    left: "-50vh"
+    top: "18em",
+    left: "-25em"
   },
+  close:{
+    position:"relative",
+    top:"100px"
+  },
+
   formButton: {
     width: "280px",
     background: "rgba(112, 112, 112, 0.5)",
@@ -44,7 +55,7 @@ const styles = () => ({
     cursor: "pointer",
     fontSize: "1.5rem",
     position: "relative",
-    top: "-16vh"
+    top: "-7em"
   },
   passwordForm: {
     backgroundColor: "rgba(112, 112, 112, 0.5)",
@@ -53,48 +64,52 @@ const styles = () => ({
     color: "#0085C7",
     padding: "1em 1em 1em 1em",
     position: "relative",
-    top: "-14vh"
+    top: "-9.5em"
   },
   callContainer: {
     position: "relative",
-    top: "-17.25vh"
+    top: "-180px",
+    left:"37px",
+    paddingTop:"3.3rem"
   },
   voicemailContainer: {
-    height: "7.75vh"
+    height: "3.25em",
+    paddingTop:"0em",
+    marginBottom:"0.75rem"
   },
   phoneNumberText: {
     fontSize: "17.5px",
     position: "relative",
-    top: "1.6vh",
-    left: "24vh",
+    top: "1em",
+    left: "9.5em",
     fontWeight: "500",
   },
   locationText: {
     fontSize: "13px",
     color: "#8A8A8D",
     position: "relative",
-    top: "1.25vh",
-    left: "24vh",
+    top: "1.3em",
+    left: "13em",
   },
   dayText: {
     fontSize: "13px",
     color: "#8A8A8D",
     position: "relative",
-    top: "2vh",
-    right: "-37vh",
+    top: "1.3em",
+    right: "-20em",
   },
   timeText: {
     fontSize: "13px",
     color: "#8A8A8D",
     position: "relative",
-    top: "2vh",
-    right: "-39vh",
+    top: "1.3em",
+    right: "-21em",
     letterSpacing: "1.5px"
   },
   infoIcon: {
     position: "relative",
-    top: "1.5vh",
-    right: "-37vh"
+    top: "0.6em",
+    right: "-10.7em"
   },
   voicemailPopup: {
     background: `url(${puzzle3.popup})`,
@@ -123,7 +138,8 @@ const styles = () => ({
   transcriptionText: {
     fontSize: "16px",
     color: "#626262"
-  },
-});
+  }
 
+  
+});
 export default styles;
